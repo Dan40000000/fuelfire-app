236b2306e9d832e47171c70a5d245aeffd4e64ff
Dan Perry <danperry@example.com>
2026-08-17 14:33:31 -0600

    Install Node in Xcode Cloud

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -4,5 +4,10 @@ set -eu
 
 cd "${CI_PRIMARY_REPOSITORY_PATH:-$(pwd)}"
 
+if ! command -v npm >/dev/null 2>&1; then
+  export HOMEBREW_NO_AUTO_UPDATE=1
+  brew install node
+fi
+
 npm ci
 npx cap sync ios
